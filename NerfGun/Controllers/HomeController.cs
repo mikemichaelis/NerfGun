bfa3133d4c8acc3adb8738680452cc22d1b690fa
XSD Consulting <admin@xsdconsulting.com>
2014-07-10 14:39:47 -0400

    Update

---
+++ NerfGun/Controllers/HomeController.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace NerfGun.Controllers
+{
+    public class HomeController : Controller
+    {
+        public ActionResult Index()
+        {
+            ViewBag.Message = "Data Collector";
+
+            return View();
+        }
+
+        public ActionResult About()
+        {
+            ViewBag.Message = "Data Collector provides allows the collection of data with an associated location";
+
+            return View();
+        }
+
+        public ActionResult Contact()
+        {
+            ViewBag.Message = "mike@xsdconsulting.com";
+
+            return View();
+        }
+    }
+}
